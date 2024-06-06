c26cde067c5f7b81fe6a27d23103e98d9b6abdb2
kushagrasharma-13 <sharmakushagra295@gmail.com>
2024-06-07 00:40:35 +0530

    Logistics_Bot

--- logistics_bot.py
+++ logistics_bot.py
@@ -4,8 +4,6 @@ import openai
 import requests
 from datetime import datetime
 from dotenv import load_dotenv
-from Flight_PDF_Generator import ItineraryPDFGenerator
-from Hotel_PDF_Generator import HotelPDFGenerator
 
 load_dotenv()
 
@@ -21,6 +19,8 @@ Extract the following information and adhere strictly to the given output format
 3. Start Date
 4. End Date
 5. Count of Crew Members
+6. Start location airport code
+7. End location airport code
 
 Text: {text}
 
@@ -31,6 +31,8 @@ Please provide the output in the exact format below:
 3. Start Date:
 4. End Date:
 5. Count of Crew Members:
+6. Start location airport code:
+7. End location airport code:
 """
 
 def extract_travel_information(text):
@@ -50,24 +52,18 @@ def extract_travel_information(text):
 
     response_text = response.choices[0].message.content
     lines = response_text.split('\n')
-    # print(lines)
     start_location = lines[0].split("Start Location:")[1].strip()
     end_location = lines[1].split("End Location:")[1].strip()
     start_date = lines[2].split("Start Date:")[1].strip()
     end_date = lines[3].split("End Date:")[1].strip()
     crew_members = lines[4].split("Count of Crew Members:")[1].split()[0].strip()
+    start_location_airport_code = lines[5].split("Start location airport code:")[1].strip()
+    end_location_airport_code = lines[6].split("End location airport code:")[1].strip()
 
     
-    return start_location, end_location, start_date, end_date, crew_members
+    return start_location, end_location, start_date, end_date, crew_members, start_location_airport_code, end_location_airport_code
 
-start_location, end_location, start_date, end_date, crew_members = extract_travel_information(user_input)
-# print(start_location)
-# print(end_location)
-# print(start_date)
-# print(end_date)
-# print(start_location_airport_code)
-# print(end_location_airport_code)
-# print(crew_members)
+start_location, end_location, start_date, end_date, crew_members, start_location_airport_code, end_location_airport_code = extract_travel_information(user_input)
 
 def format_date(date_str):
     try:
@@ -81,55 +77,19 @@ def format_date(date_str):
 start_date = format_date(start_date)
 end_date = format_date(end_date)
 
-# print(start_date)
-# print(end_date)
 
-
-# Searching for Flights
-
-flightID_url = "https://sky-scanner3.p.rapidapi.com/flights/auto-complete"
-flightID_headers = {
-	"X-RapidAPI-Key": x_rapid_api_Key,
-	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
-}
-
-flightID_querystring = {"query":start_location}
-flightID_response = requests.get(flightID_url, headers=flightID_headers, params=flightID_querystring)
-fromID = flightID_response.json()['data'][0]['presentation']['skyId']
-
-flightID_querystring = {"query":end_location}
-flightID_response = requests.get(flightID_url, headers=flightID_headers, params=flightID_querystring)
-toID = flightID_response.json()['data'][0]['presentation']['skyId']
-with open("flightID.json", 'w', encoding='utf-8') as flightID_file:
-    json.dump(flightID_response.json(), flightID_file, indent=4)
-
-# print(flightID_response)
-# print(fromID)
-# print(toID)
-
+## Searching for Flights
 
 flight_url = "https://sky-scanner3.p.rapidapi.com/flights/search-roundtrip"
-flight_querystring = {"fromEntityId":fromID, "toEntityId":toID, "departDate":start_date, "returnDate":end_date, "currency":"USD"}
+flight_querystring = {"fromEntityId":start_location_airport_code, "toEntityId":end_location_airport_code, "departDate":start_date, "returnDate":end_date, "currency":"USD"}
 flight_headers = {
 	"X-RapidAPI-Key": x_rapid_api_Key,
 	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
 }
 flight_response = requests.get(flight_url, headers=flight_headers, params=flight_querystring)
-# print("\n\n##################### FLIGHT JSON ################################\n\n")
-# print(flight_response.json())
-# print("\n\n")
-
-with open("flight.json", 'w', encoding='utf-8') as flight_file:
-    json.dump(flight_response.json(), flight_file, indent=4)
-    
-
-generator = ItineraryPDFGenerator(flight_response.json())
-generator.generate_pdf("Flight_Data.pdf")
-
 
 
 ## Searching for Hotels
-
 
 hotelID_url = "https://sky-scanner3.p.rapidapi.com/hotels/auto-complete"
 hotelID_querystring = {"query":end_location}
@@ -138,12 +98,8 @@ hotelID_headers = {
 	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
 }
 hotelID_response = requests.get(hotelID_url, headers=hotelID_headers, params=hotelID_querystring)
-# print(hotelID_response.json())
-with open("hotelID.json", 'w', encoding='utf-8') as hotelID_file:
-    json.dump(hotelID_response.json(), hotelID_file, indent=4)
 
 entity_ID = hotelID_response.json()['data'][0]['entityId']
-
 
 hotel_url = "https://sky-scanner3.p.rapidapi.com/hotels/search"
 hotel_querystring = {"entityId":entity_ID,"checkin":start_date,"checkout":end_date}
@@ -152,13 +108,6 @@ hotel_headers = {
 	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
 }
 hotel_response = requests.get(hotel_url, headers=hotel_headers, params=hotel_querystring)
-
-with open("hotel.json", 'w', encoding='utf-8') as hotel_file:
-    json.dump(hotel_response.json(), hotel_file, indent=4)
-
-
-pdf_generator = HotelPDFGenerator(hotel_response.json())
-pdf_generator.generate_pdf()
 
 
 # taxiID_url = "https://sky-scanner3.p.rapidapi.com/cars/auto-complete"
