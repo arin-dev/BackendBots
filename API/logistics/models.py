602b9899fb94c40455c2b6dc3b2e5aade01534ba
kushagrasharma-13 <sharmakushagra295@gmail.com>
2024-06-19 14:18:54 +0530

    Re-migrated

--- API/logistics/models.py
+++ API/logistics/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Logistics(models.Model):
     project = models.ForeignKey('project.Project', on_delete=models.CASCADE, related_name='logistics_set')
     flights_details = models.JSONField()
     hotel_details = models.JSONField()
     taxi_details = models.JSONField()
 
-    
+    def __str__(self):
+        return f"Logistics for {self.project}"
